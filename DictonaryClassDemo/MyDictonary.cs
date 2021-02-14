ab90e12f3f2909fa04d2ffe119abf0d605070f6f
Armagan Şahin <armi1995@hotmail.com>
2021-02-15 00:06:14 +0300

    Add project files.

---
+++ DictonaryClassDemo/MyDictonary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictonaryClassDemo
+{
+    class MyDictonary<V,K>
+    {
+        private V[] _value;
+        private K[] _key;
+        private V[] _tempValue;
+        private K[] _tempKey;
+
+        public MyDictonary()
+        {
+            _value = new V[0];
+            _key = new K[0];
+        }
+
+        public void Add(V value, K key)
+        {
+            _tempValue = _value;
+            _tempKey = _key;
+            _value = new V[_value.Length + 1];
+            _key = new K[_key.Length + 1];
+            
+            for (int i = 0; i < _tempValue.Length; i++)
+            {
+                _value[i] = _tempValue[i];
+                _key[i] = _tempKey[i];
+            }
+
+            _value[_value.Length - 1] = value;
+            _key[_key.Length - 1] = key;
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < _value.Length; i++)
+            {
+                Console.WriteLine(_value[i]+" : "+_key[i]);
+            }
+        }
+        
+    }
+}
---
+++ DictonaryClassDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictonaryClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictonary<string, string> metallicaList = new MyDictonary<string, string>();
+            
+            metallicaList.Add("Vocal and Rhythm Guitar","James Hetfield");
+            metallicaList.Add("Drummer","Lars Ulrich");
+            metallicaList.Add("Lead Guitar","Kirk Hammet");
+            metallicaList.Add("Bass Guitar and Back Vocals","Robert Trujillo");
+            metallicaList.List();
+
+            Console.WriteLine("-------------------------------------------------");
+
+            MyDictonary<int, string> studentList = new MyDictonary<int, string>();
+            studentList.Add(1,"Armağan Şahin");
+            studentList.Add(2,"Anıl Şahin");
+            studentList.Add(3,"Zeynep Öztürk");
+
+            studentList.List();
+
+        }
+    }
+}
